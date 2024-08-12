495e7891b1440e5ed765cff9d3d0d893c59bf20e
gaspar bucuane <gasparbucuane@gmaiil.com>
2024-08-12 16:23:17 +0200

    first commit

---
+++ usuarios/urls.py
@@ -0,0 +1,8 @@
+# urls.py
+from django.urls import path
+from .views import cadastro, logar
+
+urlpatterns = [
+    path('cadastro/', cadastro, name='cadastro'),
+    path('logar/', logar, name='logar'),
+]
